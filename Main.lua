2cdbb1c0bcc93f8ab4de03918848306ac608fc6b
couldntBeT <148791759+couldntBeT@users.noreply.github.com>
2024-10-21 00:27:24 +0100

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,6 @@
+local omg = {
+    [8204913] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/UltimateTowerDefense.lua',
+    [33694454] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/PixelTowerDefense.lua',
+}   
+
+loadstring(game:HttpGet(omg[game.CreatorId]))()
