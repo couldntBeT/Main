9095f3cb1abe72b518538349f3e4cdb296f8a50f
couldntBeT <148791759+couldntBeT@users.noreply.github.com>
2024-11-30 16:22:13 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3,6 +3,7 @@ local omg = {
     [33694454] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/PixelTowerDefense.lua',
     [33781793] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/AnimeShadow.lua',
     [32380537] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/BallTowerDefense.lua',
+	[344278439] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/HorrorTowerDefense.lua'
 }   
 
 loadstring(game:HttpGet(omg[game.CreatorId]))()
