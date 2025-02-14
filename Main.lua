d8068644dcb36520ec3b48170956f9e61982f1ed
couldntBeT <148791759+couldntBeT@users.noreply.github.com>
2025-02-14 02:23:17 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -4,10 +4,10 @@ local omg = {
     [33781793] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/AnimeShadow.lua',
     [32380537] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/BallTowerDefense.lua',
     [34427843] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/HorrorTowerDefense.lua',
-    [33298472] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/FiveNightsTD.lua',
+    [33298472] = 'https://api.luarmor.net/files/v3/loaders/313817b9be0842382c1ca85c3f1e0483.lua',
     [14733518] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/TowerDefenseX.lua',
     [35173505] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/GoFishing.lua',
-    [34990762] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/SpongebobTowerDefense.lua',
+    [34990762] = 'https://api.luarmor.net/files/v3/loaders/955be948b5acf58d6bb947058d21eea4.lua',
 
 }   
 
