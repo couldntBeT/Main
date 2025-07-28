ee2e6d72d0a4757465fc736fcaef6c588538ff7d
couldntBeT <148791759+couldntBeT@users.noreply.github.com>
2025-07-28 19:44:48 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,23 +1,9 @@
 repeat task.wait() until game:IsLoaded()
-local keyg = {33298472}
-if table.find(keyg,game.CreatorId) and (not script_key or script_key and string.len(script_key) <= 8) then
-    game:GetService("StarterGui"):SetCore("SendNotification",{Title = "INVALID KEY",Text = "TUTORIAL COPIED TO CLIPBOARD",})
-    setclipboard("https://www.youtube.com/watch?v=LovYm9JtlZI")
-    return
-elseif not table.find(keyg,game.CreatorId) and script_key then script_key = nil
-end
-local omg = {
-    [8204913] = 'https://api.luarmor.net/files/v3/loaders/eca6b58ce796803391df2338fc566f7a.lua',
-    [33694454] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/PixelTowerDefense.lua',
-    [33781793] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/AnimeShadow.lua',
-    [32380537] = 'https://api.luarmor.net/files/v3/loaders/bca2ee0a76cb1ae0d8710859f2b9b056.lua',
-    [34427843] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/HorrorTowerDefense.lua',
-    [33298472] = 'https://api.luarmor.net/files/v3/loaders/313817b9be0842382c1ca85c3f1e0483.lua',
-    [14733518] = 'https://api.luarmor.net/files/v3/loaders/7d996863caef9a2fe732e8d65f1f9285.lua',
-    [35173505] = 'https://raw.githubusercontent.com/couldntBeT/Main/refs/heads/main/GoFishing.lua',
-    [34990762] = 'https://api.luarmor.net/files/v3/loaders/955be948b5acf58d6bb947058d21eea4.lua',
-    [5522949] = 'https://api.luarmor.net/files/v3/loaders/80d36647da6644ac1a80fbe8da4e831c.lua', -- RCU
+local Scripts = {
+    [8204913] = 'https://api.luarmor.net/files/v3/loaders/a14808b3e0f1fc95d6d12bcd1213db12.lua', -- UTD
+    [14733518] = 'https://api.luarmor.net/files/v3/loaders/234aff7558979b30f95679b80652df00.lua', -- TDX
+    [34990762] = 'https://api.luarmor.net/files/v3/loaders/453c91d69f95d4217cbdccd70fa6b2c8.lua', -- SBTD
 
 }   
 
-loadstring(game:HttpGet(omg[game.CreatorId]))()
+loadstring(game:HttpGet(Scripts[game.CreatorId]))()
