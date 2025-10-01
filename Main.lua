9cff2e42659deebfa542c7f3177aec0f3f0a9403
couldntBeT <148791759+couldntBeT@users.noreply.github.com>
2025-10-01 16:53:14 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,8 @@
 repeat task.wait() until game:IsLoaded()
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Garden TD testers",Text = "GTD Hub coming soon, If you play GTD and want to test DM me",})
+task.wait()
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "DISCORD: ashed23",Text = "",})
+
 local Scripts = {
     [8204913] = 'https://api.luarmor.net/files/v3/loaders/a14808b3e0f1fc95d6d12bcd1213db12.lua', -- UTD
     [14733518] = 'https://api.luarmor.net/files/v3/loaders/234aff7558979b30f95679b80652df00.lua', -- TDX
