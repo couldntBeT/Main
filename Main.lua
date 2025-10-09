35ba8b2b80e213e0376186d1a2142c0c67632613
couldntBeT <148791759+couldntBeT@users.noreply.github.com>
2025-10-09 15:39:02 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,4 @@
 repeat task.wait() until game:IsLoaded()
-game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Garden TD New Hub",Text = "Discord copied to clipboard (GTD ONLY)",})
-setclipboard("https://discord.gg/qV3fSpgrDe")
 local Scripts = {
     [8204913] = 'https://api.luarmor.net/files/v3/loaders/a14808b3e0f1fc95d6d12bcd1213db12.lua', -- UTD
     [14733518] = 'https://api.luarmor.net/files/v3/loaders/234aff7558979b30f95679b80652df00.lua', -- TDX
